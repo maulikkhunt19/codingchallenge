6b1374de048809fdfd8e054ca037f5ac44be4009
maulikkhunt19 <maulikkhunt8@gmail.com>
2022-05-19 11:03:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <h1>{{ title }}</h1>
+    <Imagedata />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Imagedata from "./components/imageView.vue";
+
+@Options({
+  components: {
+    Imagedata,
+  },
+})
+export default class App extends Vue {
+  public title = "Image List";
+}
+</script>
+
+<style>
+.app h1 {
+  text-align: center;
+  color: #7c795d;
+  font-family: "Trocchi", serif;
+  font-size: 45px;
+  font-weight: normal;
+  line-height: 48px;
+  margin: 0;
+  margin-top: 20px;
+}
+</style>
---
+++ src/components/imageView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="gallery">
+    <div class="gallery-panel" v-for="item in imageData" :key="item.id">
+      <img v-bind:src="item.download_url" alt="" class="image" />
+      <div class="image-caption">
+        <p class="author">{{ item.author }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  name: "imageData",
+  components: {},
+  data() {
+    return { imageData: undefined };
+  },
+  mounted() {
+    axios.get("https://picsum.photos/v2/list?page=2&limit=12").then((resp) => {
+      this.imageData = resp.data;
+      console.log(resp.data);
+    });
+  },
+});
+</script>
+
+<style>
+.gallery {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  grid-gap: 1rem;
+  max-width: 80rem;
+  margin: 5rem auto;
+  padding: 0 5rem;
+}
+
+.gallery-panel img {
+  width: 100%;
+  height: 90%;
+  object-fit: cover;
+  border-radius: 0.75rem;
+}
+.author {
+  text-align: center;
+  color: #7a7c7f;
+  font-family: "Libre Baskerville", serif;
+}
+</style>
